e06a1f6cc15688bffb4382b6283fda7583702298
Jonathan Hogue <jonathan.hogue@live.com>
2021-10-26 14:27:48 -0400

    Initial Commit

---
+++ SwiftUI UI Practice/SwiftUI_UI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UI_PracticeApp.swift
+//  SwiftUI UI Practice
+//
+//  Created by Jonathan Hogue on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
